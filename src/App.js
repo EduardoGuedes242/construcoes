70ad1a682f365f91be6e97f4697b7e3e1ac38c7a
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-11-25 07:39:25 -0300

    implementacao: implementação do componente proprio dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 import BotaoProrio from './widgets/botao';
+import DropBox from './widgets/dropdown';
 import EditProprio from './widgets/edit';
 
 function App() {
+
+  const [descricao, setDescricao] = useState('');
+
+  function funcao() {
+    console.log('teste: ' + descricao)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <input value={descricao} onChange={e => setDescricao(e.target.value)}></input>
         <EditProprio title={'Descrição'} />
+        <p></p>
+        <DropBox></DropBox>
         <p></p>
         <EditProprio title={'Largura'} />
         <p>X</p>
         <EditProprio title={'Altura'} />
         <p></p>
-        <BotaoProrio title={'Calcular'} />
+        <BotaoProrio title={'Calcular'} onClick={funcao} />
       </header>
     </div>
   );
 }
-
-
 export default App;
+
+
---
+++ src/widgets/dropdown.js
@@ -0,0 +1,9 @@
+export default function DropBox() {
+    return <>
+        <select>
+            <option>teste 01</option>
+            <option>teste 02</option>
+            <option>teste 03</option>
+        </select>
+    </>
+}
